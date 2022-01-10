88712d04d05212de8fd15b0036c3558ca7d54d01
Jeromkeee <altcreaw@gmail.com>
2022-01-10 16:34:46 +0300

    complete func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let i = 0;
+  let result = '';
+  let str_n = String(Math.abs(n));
+  while (i < str_n.length) {
+    result = `${str_n.charAt(i)}${result}`;
+    i = i + 1;
+  }
+  return result
 }
